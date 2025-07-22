9ead320694c7c90d49090c333b2a954fb66391c7
Lokesh <lokeshjourneys@gmail.com>
2025-07-22 13:16:17 +0530

    Update dependencies and enhance project router to support file uploads

--- backend/routes/Project.router.js
+++ backend/routes/Project.router.js
@@ -7,16 +7,17 @@ import {
     deleteProject
 } from "../controllers/Project.controller.js";
 import { verifyJwt, isUserAdmin } from "../middlewares/auth.middlewares.js";
+import { upload } from "../middlewares/multer.middleware.js";
 
 const projectRouter = express.Router();
 
-projectRouter.post("/", verifyJwt, isUserAdmin, createProject);
+projectRouter.post("/", verifyJwt, isUserAdmin,upload.single("photo"), createProject);
 
 projectRouter.get("/", getAllProjects);
 
 projectRouter.get("/:projectId", getProjectById);
 
-projectRouter.put("/:projectId", verifyJwt, isUserAdmin, updateProjectById);
+projectRouter.put("/:projectId", verifyJwt, isUserAdmin, upload.single("photo"), updateProjectById);
 
 projectRouter.delete("/:projectId", verifyJwt, isUserAdmin, deleteProject);
 
